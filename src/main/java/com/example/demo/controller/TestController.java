12f448b273108dd52694a1c1ea6c89c5eb94dad2
hexi <https://github.com/zwjoie/java_one_service.git>
2018-09-20 10:50:07 +0800

    新建项目

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/hi")
+    public String hi(String name){
+        return "hello "+name;
+    }
+
+}
