ad96c8daad64e2e403376e8aa315da901c8e6b4e
zhanwang <1940360144@qq.com>
2018-09-30 10:22:41 +0800

    科技部

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +22,8 @@ public class TestController {
     public  String abc(){
         return "奥斯卡了的纳税爱上了多年";
     }
-
+    @RequestMapping("/hh")
+    public String hhh(String name){
+        return "adas拉斯科镍拉丝asda: "+name;
+    }
 }
