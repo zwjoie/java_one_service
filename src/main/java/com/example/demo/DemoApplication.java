d2ee46cdd3c888f73381bc997a4ee26b05286540
zhanwang <1940360144@qq.com>
2018-09-30 16:13:05 +0800

    @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class DemoApplication {
 
 	public static void main(String[] args) {
